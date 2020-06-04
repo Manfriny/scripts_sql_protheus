4d84ad8ccd5273b715c5840e8caccee4253ae56c
Manfriny <manfriny@gmail.com>
2020-06-04 10:08:59 -0300

    20200604

--- EST - EXCLUIR INVENTARIO.sql
+++ EST - EXCLUIR INVENTARIO.sql
@@ -1,13 +1,13 @@
 define filial='0301';
-define armazem = '24';
-define dat = '20200129';
+define armazem = '21';
+define dat = '20200602';
 
---EXCLUIR O INVENTARIO
-select * from sb7010 where b7_filial='&filial' and b7_local in ('&armazem') and b7_data = '&dat' and d_e_l_e_t_=' ' order by b7_data desc;
---update sb7010 set d_e_l_e_t_='*' where b7_filial='&filial' and b7_local in ('&armazem') and b7_data = '&dat' and d_e_l_e_t_=' ';
+--EXCLUIR A IMPORTAÇAO INVENTARIO
+select * from sb7010 where b7_filial='&filial' and b7_local in ('21','22') and b7_data = '&dat' and d_e_l_e_t_=' ' order by b7_data desc;
+--update sb7010 set d_e_l_e_t_='*' where b7_filial='&filial' and b7_local in ('21','22') and b7_data = '&dat' and d_e_l_e_t_=' ';
 
 --=======================================================================================================
-define numContagem = '0032';
+define numContagem = '0036';
 
 --EXCLUIR UMA CONTAGEM DENTRO DE UM INVENTÁRIO
 select * from zs3010 where zs3_inve='&numContagem' and zs3_cont in ('&armazem');
@@ -16,4 +16,4 @@ select * from zs3010 where zs3_inve='&numContagem' and zs3_cont in ('&armazem');
 
 --EXCLUIR ITENS DE UMA CONTAGEM
 select * from zs4010 where ZS4_FILIAL='&filial' and ZS4_INVE='&numContagem' and ZS4_DATA='&dat' and d_e_l_e_t_=' ';
---update zs4010 set d_e_l_e_t_='*' where ZS4_FILIAL='&filial' and ZS4_INVE='&numContagem' and ZS4_DATA='&dat' and d_e_l_e_t_=' ';
+--update zs4010 set d_e_l_e_t_=' ' where ZS4_FILIAL='&filial' and ZS4_INVE='&numContagem' and ZS4_DATA='&dat' and d_e_l_e_t_='*';
