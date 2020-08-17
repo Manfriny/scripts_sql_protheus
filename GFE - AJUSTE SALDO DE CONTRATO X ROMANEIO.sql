3f1d6f54713e541c8b0bb9c61ba0e071261b1413
Manfriny <manfriny.pereira@grupotecagro.com>
2020-08-17 11:27:03 -0300

    20200817

--- GFE - AJUSTE SALDO DE CONTRATO X ROMANEIO.sql
+++ GFE - AJUSTE SALDO DE CONTRATO X ROMANEIO.sql
@@ -2,14 +2,13 @@
 select njj_codctr,a.* from njj010 a where njj_filial='0401' and njj_codrom like '%26911%' and d_e_l_e_t_=' ';
 
 --verifico a autorização do contrato
-select njp_qtdfco,njp_qtdfis,a.* from njp010 a where njp_filial='0302' and njp_codctr='000635' and njp_item='005' and d_e_l_e_t_=' ';
+select njp_qtdfco,njp_qtdfis,a.* from njp010 a where njp_filial='0401' and njp_codctr='002111' and njp_item='007' and d_e_l_e_t_=' ';
 
 --Ajusto a Qtd Fisica e o Qtd Saldo Aut conforme quant correta informada pela Comercialização
-update njp010 set njp_qtdfco=(njp_qtdfco-959160),njp_qtdfis=(njp_qtdfis-959160) where njp_filial='0302' and njp_codctr='000635' and njp_item='005' and d_e_l_e_t_=' ';
+update njp010 set njp_qtdfco=(njp_qtdfco-50410),njp_qtdfis=(njp_qtdfis-50410) where njp_filial='0401' and njp_codctr='002111' and njp_item='007' and d_e_l_e_t_=' ';
 
 
 
 /*
-select NJR_QTSFCO,NJR_QTSFIS,njr_qslfco,NJR_SLDFIS,a.* from njr010 a where njr_filial='0401' and njr_codctr='001890';
-update njr010 set NJR_QTSFCO=1904000,NJR_QTSFIS=1904000,njr_qslfco=195200,NJR_SLDFIS=195200 where njr_filial='0401' and njr_codctr='001890';
-select * from njp010 where njp_filial='0401' and njp_codctr='001890';
+NJP_QTDAUT - o valor que a autorização tem que ficar.. 
+NJP_QTDFCO - o resultado acima...
